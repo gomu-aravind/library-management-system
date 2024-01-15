b019fa870e125e248529584064b977b73d968178
Gomu Aravind <aravindsubramanian159@gmail.com>
2024-01-15 22:14:13 +0530

    Changes frontend

--- client/src/components/Home/Header.jsx
+++ client/src/components/Home/Header.jsx
@@ -19,7 +19,11 @@ function Header() {
       const resData = await fetch(
         import.meta.env.VITE_SERVER_URL + "/user/logout",
         {
-          method: "GET",
+          method: "POST",
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify({logout:true}),
           credentials: "include",
         }
       );
